4fa63bdfe87a31545f9e4d1abce973eb47f0f3c5
Nich <nctrenco@gmail.com>
2018-07-08 13:26:56 +0800

    santoni: Revert "QCamera2: For supporting VNDK and Treble rules."
    Google added the CameraParameters class to the camera helper, doing
    away with the need for importing the CameraParameters source files.
    https://android.googlesource.com/platform/hardware/interfaces/+/b9b59bdca47dbf1d94a586527f7d139ed50c90d7
    
    Let's revert this patch and pick the one from Google fresh instead.
    
    This reverts commit 667dcfbd62d4ba12ddd0b419ae9a8c86918480c1.
    (Removal of QCameraDisplay from compilation and libgui in the reverted
    commit was retained)
    
    Change-Id: I5f38e333090e691c46f65f80a3f0ee142bdde9dc

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -46,7 +46,6 @@ LOCAL_SRC_FILES += \
         HAL/QCameraPostProc.cpp \
         HAL/QCamera2HWICallbacks.cpp \
         HAL/QCameraParameters.cpp \
-        HAL/CameraParameters.cpp \
         HAL/QCameraParametersIntf.cpp \
         HAL/QCameraThermalAdapter.cpp
 endif
@@ -115,7 +114,7 @@ LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
         $(call project-path-for,qcom-display)/libqservice
-LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
+LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 LOCAL_SHARED_LIBRARIES += libcutils libdl
@@ -123,7 +122,6 @@ ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 
-LOCAL_STATIC_LIBRARIES := android.hardware.camera.common@1.0-helper
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_VENDOR_MODULE := true
