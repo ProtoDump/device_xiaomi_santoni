ac8e3c7b070c2a55d278a5b97f8eef8e6ef68d6d
Nich <nctrenco@gmail.com>
2019-02-12 06:37:55 +0000

    santoni: Switch to FPC HAL from MM
     * From Xiaomi/santoni/santoni:6.0.1/MMB29M/V8.5.4.0.MAMCNED:user/release-keys
     * The FPC HALs from Nougat were _extremely_ buggy:
      - broken enumeration
      - non-standard acquisition info
      - HAL would continuously authenticate (with the finger left at the
        sensor) despite already having successfully done so
     * Furthermore post-8.2.1 FPC HALs have issues with enrolling the first
       fingerprint (thanks to ryan-andri for bringing my attention to this)
     * Instead of just rolling back to pre-8.2.1 HAL like in commit 4a5a2e9,
       roll back to the FPC HAL from Marshmallow so that the other bugs are
       addressed.
    
    Change-Id: I10fe899e425b5decb9b0fc78ed76048fd2f93a89

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.custom
+    android.hardware.biometrics.fingerprint@2.0-service.custom
 
 # Init scripts
 PRODUCT_PACKAGES += \
